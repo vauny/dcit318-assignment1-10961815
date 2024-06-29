68783774d5d4e19f0b84dddf21b4342447bbb97d
vauny <vndhammond001@st.ug.edu.gh>
2024-06-29 21:36:16 +0000

    Triangular Type Identifier

---
+++ TriangularTypeIdentifier/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Enter the length of the first side: ");
+        int side1 = GetValidatedSide();
+
+        Console.Write("Enter the  of the second side: ");
+        int side2 = GetValidatedSide();
+
+        Console.Write("Enter the length of the third side: ");
+        int side3 = GetValidatedSide();
+
+        if (side1 == side2 && side2 == side3)
+        {
+            Console.WriteLine("The triangle is Equilateral.");
+        }
+        else if (side1 == side2 || side2 == side3 || side1 == side3)
+        {
+            Console.WriteLine("The triangle is Isosceles.");
+        }
+        else
+        {
+            Console.WriteLine("The triangle is Scalene.");
+        }
+    }
+
+    static int GetValidatedSide()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int side))
+            {
+                return side;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
